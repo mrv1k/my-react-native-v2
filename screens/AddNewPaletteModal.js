8e1bd3e55e8257da3f700427ea337e8d620b6035
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-03-27 13:26:17 -0400

    wip7: add TextField and Submit, separate colors

--- screens/AddNewPaletteModal.js
+++ screens/AddNewPaletteModal.js
@@ -1,5 +1,14 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import {FlatList, StyleSheet, Switch, Text, View} from 'react-native';
+import {
+  FlatList,
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import COLORS from '../data/colors';
 
 const ColorSwitch = ({color, updateSelectedColors}) => {
   const [isEnabled, setIsEnabled] = useState(false);
@@ -18,7 +27,8 @@ const ColorSwitch = ({color, updateSelectedColors}) => {
   );
 };
 
-const AddNewPaletteModal = () => {
+export default function AddNewPaletteModal() {
+  const [paletteName, setPaletteName] = useState('');
   const [selectedColors, setSelectedColors] = useState([]);
 
   const updateSelectedColors = useCallback((switchIsEnabled, color) => {
@@ -41,22 +51,42 @@ const AddNewPaletteModal = () => {
   return (
     <View style={styles.global}>
       <FlatList
-        data={selectedColors}
-        renderItem={({item}) => <Text>{item.colorName}</Text>}
-        keyExtractor={(item) => item.hexCode}
-      />
-      <FlatList
         data={COLORS}
         renderItem={renderItem}
         keyExtractor={(item) => item.colorName}
         ItemSeparatorComponent={() => <View style={styles.cellSeparator} />}
+        ListHeaderComponent={
+          <View style={styles.header}>
+            <Text>Name of your color palette</Text>
+            <TextInput
+              value={paletteName}
+              onChangeText={setPaletteName}
+              style={styles.input}
+            />
+          </View>
+        }
+        ListFooterComponent={
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Submit!</Text>
+          </TouchableOpacity>
+        }
       />
     </View>
   );
-};
+}
 
+const grayColor = 'rgb(196, 197, 197)';
 const styles = StyleSheet.create({
   global: {paddingHorizontal: 10, backgroundColor: '#fff'},
+  header: {paddingVertical: 10},
+  input: {
+    marginVertical: 10,
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 10,
+    borderColor: grayColor,
+    borderRadius: 5,
+  },
   cell: {
     flexDirection: 'row',
     alignContent: 'space-between',
@@ -66,26 +96,24 @@ const styles = StyleSheet.create({
   },
   cellText: {flex: 1, marginLeft: 10},
   cellSwitch: {alignSelf: 'flex-end', marginRight: 10},
-  cellSeparator: {height: 1, backgroundColor: 'rgb(196, 197, 197)'},
+  cellSeparator: {height: 1, backgroundColor: grayColor},
+  button: {
+    backgroundColor: '#78C5EF',
+    borderRadius: 5,
+    padding: 15,
+    textAlignVertical: 'center',
+    marginVertical: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
-
-const COLORS = [
-  {colorName: 'AliceBlue', hexCode: '#F0F8FF'},
-  {colorName: 'AntiqueWhite', hexCode: '#FAEBD7'},
-  {colorName: 'Aqua', hexCode: '#00FFFF'},
-  {colorName: 'Aquamarine', hexCode: '#7FFFD4'},
-  {colorName: 'Azure', hexCode: '#F0FFFF'},
-  {colorName: 'Beige', hexCode: '#F5F5DC'},
-  {colorName: 'Bisque', hexCode: '#FFE4C4'},
-  {colorName: 'Black', hexCode: '#000000'},
-  {colorName: 'BlanchedAlmond', hexCode: '#FFEBCD'},
-];
-
-export default AddNewPaletteModal;
 
 // Requirements
 // [x] pressing on "add color scheme" opens a modal
-// [] the user can enter the name for the color scheme
+// [x] the user can enter the name for the color scheme
 // [x] the user can use toggle buttons to select colors to add to the scheme
 // [] if the user hits submit without entering a name for the color, they will get an error message
 // [] if the user hits submit without entering the number of colors, they will get an error message
