b25bbc5025f2aed1e97d664765c1a337ef0f9ae7
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-04-01 17:56:47 -0400

    convert color palette

--- screens/ColorPalette.tsx
+++ screens/ColorPalette.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import {View, Text, FlatList, StyleSheet} from 'react-native';
 import ColorBox from '../components/ColorBox';
+import {ColorPaletteRouteProp} from '../types';
 
-const ColorPalette = (params) => (
+interface ColorPaletteProps {
+  route: ColorPaletteRouteProp;
+}
+const ColorPalette = ({route}: ColorPaletteProps) => (
   <View style={styles.container}>
     <FlatList
-      data={params.route.params.colors}
+      data={route.params.colors}
       renderItem={({item}) => (
         <ColorBox hexCode={item.hexCode} colorName={item.colorName} />
       )}
       keyExtractor={(item) => item.hexCode}
       ListHeaderComponent={
-        <Text style={styles.heading}>{params.route.params.paletteName}</Text>
+        <Text style={styles.heading}>{route.params.paletteName}</Text>
       }
     />
   </View>
